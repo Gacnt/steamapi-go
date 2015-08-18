357b7f8802b4bf96f14fd5e85ae738a8bf28d470
Gacnt <gacnt@shaw.ca>
2015-08-17 21:33:49 -0600

    made more fool proof

--- news.go
+++ news.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type SteamAPI struct {
@@ -16,7 +17,7 @@ type GetAppNews struct {
 	AppNews struct {
 		AppId     int `json:"appid"`
 		NewsItems []struct {
-			Gid           int    `json:"gid"`
+			Gid           string `json:"gid"`
 			Title         string `json:"title"`
 			Url           string `json:"url"`
 			IsExternalUrl bool   `json:"is_external_url"`
@@ -28,31 +29,58 @@ type GetAppNews struct {
 	} `json:"appnews"`
 }
 
-type JsonResponse map[string]GetAppNews
+func (s SteamAPI) GetNewsForApp(appid, count, maxlength int) (*GetAppNews, error) {
 
-func (s SteamAPI) GetNewsForApp(appid, count, maxlength int) error {
+	// Convert the `int` variables from
+	// the arguments supplied to the
+	// method
+
 	sAppid := strconv.Itoa(appid)
 	sCount := strconv.Itoa(count)
 	sMaxlength := strconv.Itoa(maxlength)
 
-	resp, err := http.Get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=" + sAppid + "&count=" + sCount + "&maxlength=" + sMaxlength + "&format=json")
+	// Set the appropriate URI for the
+	// API request
+
+	requestURI := "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=" + sAppid + "&count=" + sCount + "&maxlength=" + sMaxlength + "&format=json"
+
+	// Do this the proper way so that
+	// there is no error for any api
+	// requests that may take too long
+	// to return
+
+	client := &http.Client{Timeout: time.Duration(60) * time.Second}
+	req, err := http.NewRequest("GET", requestURI, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	// Send off our request to the
+	// API service
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
 	defer resp.Body.Close()
+
+	// Read from our body till EOF
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	var jsonReturn JsonResponse
+	// Get and send our data to our struct
 
-	json.Unmarshal(body, &jsonReturn)
+	var jsonResp GetAppNews
+	err = json.Unmarshal(body, &jsonResp)
+	if err != nil {
+		return nil, err
+	}
 
-	fmt.Println(jsonReturn)
-
-	return nil
+	// Everybody is happy! Send back structured data
+	return &jsonResp, nil
 
 }
